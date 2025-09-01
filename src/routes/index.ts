3d4cff3c842d27ebbe5878915646428ad06574f6
Ayush Jain <ayushj.dev@gmail.com>
2025-09-01 21:14:48 +0530

    Feature: Added not found handler

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { healthRouter } from './health.route';
+import { notFoundRouter } from './not-found.route';
 
 const router = Router();
 
 router.use('/health', healthRouter);
+
+/* Do not change position of not found router. This should always be called at last. */
+router.use(notFoundRouter);
 
 export default router;
