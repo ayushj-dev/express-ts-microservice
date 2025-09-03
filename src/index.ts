40c6722a4239a7a9fd005d898a11058bbc23a6d0
Ayush Jain <ayushj.dev@gmail.com>
2025-09-03 14:39:23 +0530

    Feature: Created a request middleware

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import express from 'express';
 import routes from './routes';
 import { errorHandler } from './middlewares/error-handler.middleware';
 import { CONFIG } from './config/config';
+import { requestMiddleware } from './middlewares/request.middleware';
 
 const app = express();
+
+app.use(requestMiddleware);
 
 app.use(CONFIG.API_PREFIX, routes);
 
