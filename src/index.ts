07ec5811f4da2df5948e37f6599f237f46388c5e
Ayush Jain <ayushj.dev@gmail.com>
2025-09-04 21:18:27 +0530

    Feature: Created a response middleware

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import routes from './routes';
 import { errorHandler } from './middlewares/error-handler.middleware';
 import { CONFIG } from './config/config';
 import { requestMiddleware } from './middlewares/request.middleware';
+import { responseMiddleware } from './middlewares/response.middleware';
 
 const app = express();
 
 app.use(requestMiddleware);
+
+app.use(responseMiddleware);
 
 app.use(CONFIG.API_PREFIX, routes);
 
