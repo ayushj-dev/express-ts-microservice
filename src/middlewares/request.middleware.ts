a2cb72941ef45c50c70523d57fecbd55935b3773
Ayush Jain <ayushj.dev@gmail.com>
2025-09-05 13:57:57 +0530

    Feature: Added logic to create request scoped context

--- src/middlewares/request.middleware.ts
+++ src/middlewares/request.middleware.ts
@@ -1,13 +1,27 @@
-import { logger } from '@/utils/logger.utils';
 import { Request, Response, NextFunction } from 'express';
+import { randomUUID } from 'crypto';
+import { asyncLocalStorage, RequestContext } from '@/context/context';
+import { logger } from '@/utils/logger.utils';
 
 export const requestMiddleware = (req: Request, res: Response, next: NextFunction) => {
-logger.info({
+  const requestId = randomUUID();
+
+  logger.info({
     method: req.method,
     url: req.originalUrl,
     userAgent: req.headers['user-agent'] ?? "Unknown",
-    ip: req.ip
+    ip: req.ip,
+    requestId
   });
 
-  next();
+  const context: RequestContext = {
+    requestId,
+  };
+
+  // Run the rest of the middleware chain and route handlers within this
+  // AsyncLocalStorage context. This is the core of request-scoped data.
+  asyncLocalStorage.run(context, () => {
+    // Add more code here according to requirement
+    next();
+  });
 }
